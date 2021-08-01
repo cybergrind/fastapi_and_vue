9dd6b2e7bcfd447268bbca2a297db619f3724ec3
Kirill Pinchuk <cybergrind@gmail.com>
2021-08-01 23:14:24 +0300

    books: generate random

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,6 +3,7 @@ from typing import Any, List
 from fastapi import FastAPI
 
 from .models import Author, Book
+from .factories import BookF
 from .utils import ORJSONResponse
 
 
@@ -18,6 +19,7 @@ AUTHORS = [
     Author(id=1, first_name='Stanislav', last_name='Lem'),
     Author(id=2, first_name='Ursula', last_name='Le Guin'),
 ]
+BOOKS = [BookF() for _ in range(10)]
 
 
 @app.get('/author')
@@ -27,4 +29,4 @@ async def author() -> List[Author]:
 
 @app.get('/book')
 async def book() -> List[Book]:
-    return ORJSONResponse({})
+    return ORJSONResponse(BOOKS)
